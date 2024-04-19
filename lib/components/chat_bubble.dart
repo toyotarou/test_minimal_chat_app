c4f537e99ebaf7e6a0644087bd022d97abf5c802
toyotarou <toyodahideyuki4819@gmail.com>
2024-04-19 22:43:42 +0900

    commit 20240419 color adjust

--- lib/components/chat_bubble.dart
+++ lib/components/chat_bubble.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../themes/theme_provider.dart';
 
 class ChatBubble extends StatelessWidget {
   const ChatBubble({super.key, required this.message, required this.isCurrentUser});
@@ -9,16 +12,24 @@ class ChatBubble extends StatelessWidget {
   ///
   @override
   Widget build(BuildContext context) {
+    final isDarkMode = Provider.of<ThemeProvider>(context, listen: false).isDarkMode;
+
     return Container(
       padding: const EdgeInsets.all(10),
       margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
       decoration: BoxDecoration(
-        color: isCurrentUser ? Colors.green : Colors.grey.shade500,
+        color: isCurrentUser
+            ? isDarkMode
+                ? Colors.green.shade500
+                : Colors.grey.shade500
+            : isDarkMode
+                ? Colors.grey.shade800
+                : Colors.grey.shade200,
         borderRadius: BorderRadius.circular(10),
       ),
       child: Text(
         message,
-        style: const TextStyle(color: Colors.white),
+        style: TextStyle(color: isDarkMode ? Colors.white : Colors.black),
       ),
     );
   }
